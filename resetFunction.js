5ca3b307712e59c5307eaef0849c024d94f6673a
thang34160 <thang4@hotmail.com>
2025-01-12 08:09:54 +0100

    changement du src js

--- resetFunction.js
+++ resetFunction.js
@@ -1,23 +1,21 @@
 let count = 0;
 
-
 function displayCount() {
-    document.getElementById('countDisplay').innerHTML=count;
+    document.getElementById('countDisplay').innerHTML = count;
 }
 
 function resetCount() {
-    count=0;
+    count = 0;
+    displayCount(); // Mettez à jour l'affichage après avoir réinitialisé
 }
 
 function checkCountValue() {
     if (count === 0) {
-        alert("Your insta count has been reset !");
+        alert("Your insta count has been reset!");
     }
 }
 
 function increaseCount() {
     count++;
     displayCount();
-    resetCount();
-    checkCountValue();
 }
